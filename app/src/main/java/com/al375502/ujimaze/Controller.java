70e063ee56eae810012f922263ed390aea487cb3
allstendres98 <allstendres@users.noreply.github.com>
2020-05-05 14:10:15 +0200

    uji maze final version

--- app/src/main/java/com/al375502/ujimaze/Controller.java
+++ app/src/main/java/com/al375502/ujimaze/Controller.java
@@ -361,7 +361,6 @@ public class Controller implements IGameController, Model.SoundPlayer {
         AllTargetsCollected = soundPool.load(context, R.raw.all_targets_collected, 0);
         TouchWall = soundPool.load(context, R.raw.touch_wall, 0);
         Reset = soundPool.load(context, R.raw.reset, 0);
-        
     }
 
     @Override
